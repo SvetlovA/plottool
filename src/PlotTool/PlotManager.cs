d78fbb1fe12a09e895f00067021594de3d17d031
douchebagsir <21arte62@gmail.com>
2021-03-18 00:28:26 +0300

    refactored

--- src/PlotTool/PlotManager.cs
+++ src/PlotTool/PlotManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.FSharp.Core;
 using Plotly.NET;
 using PlotTool.Entities;
 using PlotTool.Services;
